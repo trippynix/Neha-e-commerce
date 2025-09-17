76c00987942147b7be28c7502da3e93cbe7e52d1
unknown <ameytonannavar27@gmail.com>
2025-09-17 23:34:39 +0530

    Header partially done, assets remaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Homepage from "./pages/Homepage";
 
 function App() {
   return (
-    <>
+    <Router>
       <Header />
-      <Homepage />
-    </>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,30 @@
 import React from "react";
+import { Link } from "react-router";
 
 function Header() {
   return (
     <>
-      <header className="bg-white flex justify-around">
-        <div className="text-3xl">NR</div>
-        <div>Neha Rajput</div>
-        <div className="">
-          {/* search icon */}
-          {/* profile icon */}
-          {/* heart icon */}
-          {/* cart icon */}
-          {/* whatsapp icon */}
+      <header className="font-pfd-regular bg-white">
+        <div className="flex justify-around">
+          <div className="text-3xl">NR</div>
+          <div>Neha Rajput</div>
+          <div className="">
+            {/* search icon */}
+            {/* profile icon */}
+            {/* heart icon */}
+            {/* cart icon */}
+            {/* whatsapp icon */}
+          </div>
+        </div>
+        <div className="flex flex-col items-center">
+          <hr className="w-1/24 border-0.8 border-red-100 mb-2" />
+          <div className="flex justify-center space-x-8">
+            <Link to="/">TRADITIONAL</Link>
+            <Link to="/">WESTERN</Link>
+            <Link to="/">ACCESSORIES</Link>
+            <Link to="/">RENTALS</Link>
+            <Link to="/">READY TO SHIP</Link>
+          </div>
         </div>
       </header>
     </>
